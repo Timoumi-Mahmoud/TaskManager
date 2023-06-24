d28e408e84be466ff5bfff469d5c065da4221002
Timoumi_Mahmoud <mahmoud.timoumi@espirt.tn>
2023-06-25 01:21:56 +0200

    adding new keys

--- modeles/task.js
+++ modeles/task.js
@@ -6,12 +6,29 @@ const TaskScheme= new mongoose.Schema({
         type:String,
          required:[true, 'must provide a title ']  ,
          trim: true,
-         maxlength:[20 , 'name can not be more than 20 characters'],
+         maxlength:[10 , 'name can not be more than 10 characters'],
         }
     
     ,completed: {type: Boolean,
         default:false,
     }
+    ,description:{
+        type:String,
+       // required:[true, 'must provide a description ']  ,
+        trim: true,
+        maxlength:[50 , 'description can not be more than 50 characters'],
+      //  minlength:[10 , 'description can not be less than 10 characters'],
+
+    },
+    start:
+         Date,
+    
+
+         category : { 
+            type:String,
+             trim: true,
+            }
+        
 })
 
  module.exports=mongoose.model('Task', TaskScheme)
