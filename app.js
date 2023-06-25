06c6ed0b16e39b84b570776e691b12be7415b1a3
Timoumi_Mahmoud <mahmoud.timoumi@espirt.tn>
2023-06-25 21:00:37 +0200

    Port number

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log('Task Manager App')
 
 const connectDB =require('./db/connect')
 require('dotenv').config()
-const port=3000
+const port=process.env.PORT  || 3000
 const express=require('express')
 const app=express()
 app.use(express.static('./public'))
