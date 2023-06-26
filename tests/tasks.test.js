b262d80f214d936ac63c6158a19631832883cf65
Timoumi_Mahmoud <mahmoud.timoumi@espirt.tn>
2023-06-26 21:39:07 +0200

    Four tests

--- tests/tasks.test.js
+++ tests/tasks.test.js
@@ -17,9 +17,11 @@ beforeEach(async () => {
     await mongoose.connection.close();
   });
 
+  
+
   describe("GET /api/v1/tasks", () => {
     it("should return all Tasks", async () => {
-      const res = await request('http://localhost:3000').get("/api/v1/tasks");
+      const res = await request(app).get("/api/v1/tasks");
       expect(res.statusCode).toBe(200);
      // expect(res.body.task).toBeGreaterThan(0);
     });
@@ -27,7 +29,7 @@ beforeEach(async () => {
   
   describe("GET /api/v1/task/:id", () => {
     it("should return a task by it's id ", async () => {
-      const res = await request('http://localhost:3000').get(
+      const res = await request(app).get(
         "/api/v1/tasks/64976d69ca939c2f6837dd01"
       );
       expect(res.statusCode).toBe(200);
@@ -36,3 +38,37 @@ beforeEach(async () => {
     });
   });
 
+  
+describe("PUT /api/v1/tasks/:id", () => {
+  it("should update a task", async () => {
+    const res = await request(app)
+      .patch("/api/v1/tasks/649776aacf12d53a282917a2")
+      .send({
+        name: "task 4",
+       // description: "Description 4",
+      });
+    expect(res.statusCode).toBe(200);
+   // expect(res.body.task.name).toBe(104);
+  });
+});
+/*
+describe("DELETE /api/v1/tasks/:id", () => {
+  it("should delete a task", async () => {
+    const res = await request(app).delete(
+     "/api/v1/tasks/6498563fd230db2f5836df89"
+    );
+    expect(res.statusCode).toBe(200);
+  });
+});*/
+
+
+describe("POST /api/v1/tasks", () => {
+  it("should create a new task", async () => {
+    const res = await request(app).post("/api/v1/tasks").send({
+      name: "testJSET",
+      description: "Description 2",
+    });
+    expect(res.statusCode).toBe(201);
+    expect(res.body.task.name).toBe("testJSET");
+  });
+});
