079e619f6b72c649fb8209f572265645e48cc3f6
Timoumi_Mahmoud <mahmoud.timoumi@espirt.tn>
2023-07-05 15:40:45 +0200

    fixing the select category

--- public/quotes/quotes.js
+++ public/quotes/quotes.js
@@ -1,5 +1,7 @@
-//const btnq= document.getElementById('btnQuotes');
 const btn = document.getElementById("btnq")
+let button = document.querySelector('#button');
+let msg = document.querySelector('#message');
+var categoryName="null"
 
 btn.addEventListener("click", function(e){
 
@@ -10,51 +12,37 @@ btn.addEventListener("click", function(e){
 
 
 
-var categoryy="null"
-
-
-
 var a = document.getElementById('category');
-function getCategory(categoryy){
+function getCategoryName(){
 a.addEventListener('change', function() {
-    console.log(this.value)
-   categoryy=this.value
-   console.log("indside the funct    "+ categoryy)
-
-  return categoryy
-  console.log(categoryy)
-  //alert(this.value);
+   categoryName=this.value +""
+   categoryName=categoryName
+  return categoryName
   
 }, false);
 }
 
 
- categoryy = getCategory();
-console.log( "the value is :  "+ categoryy)
 
 
 
 
-let button = document.querySelector('#button');
-let msg = document.querySelector('#message');
 
+
+
+var categoryTwo=getCategoryName()
+  
 button.addEventListener('click', ()=>{
+    console.log(getCategoryName())
   msg.classList.toggle('reveal');
   $.ajax({
     method: 'GET',
    
-    url: 'https://api.api-ninjas.com/v1/quotes?category=' +"car" ,
+    url: 'https://api.api-ninjas.com/v1/quotes?category=' + `${categoryName}` ,
     headers: { 'X-Api-Key': 'MHif+qM6v2TdYPIKedAsng==Zb6P6CtiU610KjVF'},
     contentType: 'application/json',
     success: function(result) {
-        // console.log("result is"+ result);
-        // var quotes=JSON.stringify(result)
-        // console.log("stringfy:    "+ quotes["quote"])
-    //  var temp=Object.values(result[0].quote)
-    let temp=getCategory()
-    console.log( "the value is :  "+ temp)
-
-     console.log(result[0].quote)
+     console.log(result)
        var quoteContent= result[0].quote
        var quoteAuthor=result[0].author   
         document.getElementById("message").innerHTML = quoteContent
