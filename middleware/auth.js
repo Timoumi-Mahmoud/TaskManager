48601443d62a8a6138a21c07ceab91efd2447a7f
Timoumi_Mahmoud <mahmoud.timoumi@espirt.tn>
2023-07-10 15:11:52 +0200

    refactoring the code

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,26 @@
+const jwt = require('jsonwebtoken')
+const CustomAPIError=require('../errors/custom-error')
+
+
 
 const authenticationMiddleware= async (req,res, next)=>{
-    console.log("\n -----------\n")
-   console.log(req.headers.authorization)
-   next()
+   const authHeader= req.headers.authorization;
+   if(!authHeader || !authHeader.startsWith(`Bearer `)){
+       throw new CustomAPIError('No token provided', 401 );
+   }
+    const token =authHeader.split(' ')[1]
+   
+  try {
+    const decoded= jwt.verify(token, process.env.JWT_SECRET)
+    console.log(decoded)
+  
+    const {id, username}=decoded
+    req.user={id,username}
+  } catch (error) {
+    throw new CustomAPIError('Not autorized to acces this route', 401)
+  }
+
+    next()
 
 }
 
